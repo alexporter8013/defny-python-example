5efde52f9108375d13104ced80959f9ba82732f7
Alex Porter <bomber8013@gmail.com>
2024-04-21 22:26:37 -0700

    Filled out ideal example

--- src/lorrel_py/__init__.py
+++ src/lorrel_py/__init__.py
@@ -1,6 +1,11 @@
 from lorrel_py.__about__ import __version__
 
+# import our functions which were defined in Dafny
+from lorrel_py.Lorrel.extension import Min, Max, MinMax
 
 __all__ = [
-    "__version__"
+    "__version__",
+    "Min",
+    "Max",
+    "MinMax",
 ]
